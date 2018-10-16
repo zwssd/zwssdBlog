27f9a26109f5841725088fddfad23d6e64969ddb
David <zwssd1980@163.com>
2018-10-16 11:02:50 +0800

    修正了标签和分类页面，以及右侧热门文章字数过长的问题。

--- zwssdBlog/urls.py
+++ zwssdBlog/urls.py
@@ -2,17 +2,17 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+    Examples:
+        Function views
+        1. Add an import:  from my_app import views
+        2. Add a URL to urlpatterns:  path('', views.home, name='home')
+        Class-based views
+        1. Add an import:  from other_app.views import Home
+        2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+        Including another URLconf
+        1. Import the include() function: from django.urls import include, path
+        2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+        """
 from django.contrib import admin
 from django.urls import path
 from zwssdBlog.view import output
@@ -35,45 +35,45 @@ class StaticViewSitemap(Sitemap):
 
 sitemaps = {
     'article-is-top': GenericSitemap(
-                {
-                    'queryset': Article.objects.filter(
-                                    status=0, is_top=True
-                                ).all(),
-                    'date_field': 'pub_time'
-                },
-                priority=1.0,
-                changefreq='daily'
-            ),
+        {
+            'queryset': Article.objects.filter(
+                status=0, is_top=True
+            ).all(),
+            'date_field': 'pub_time'
+        },
+        priority=1.0,
+        changefreq='daily'
+    ),
     'article-is-not-top': GenericSitemap(
-                {
-                    'queryset': Article.objects.filter(status=0).all(),
-                    'date_field': 'pub_time'
-                },
-                priority=0.8,
-                changefreq='daily'
-            ),
+        {
+            'queryset': Article.objects.filter(status=0).all(),
+            'date_field': 'pub_time'
+        },
+        priority=0.8,
+        changefreq='daily'
+    ),
     'news': GenericSitemap(
-                {
-                    'queryset': News.objects.all(),
-                    'data_field': 'pub_time'
-                },
-                priority=0.6,
-                changefreq='daily'
-            ),
+        {
+            'queryset': News.objects.all(),
+            'data_field': 'pub_time'
+        },
+        priority=0.6,
+        changefreq='daily'
+    ),
     'category': GenericSitemap(
-                {
-                    'queryset': Category.objects.all()
-                },
-                priority=0.9,
-                changefreq='daily'
-            ),
+        {
+            'queryset': Category.objects.all()
+        },
+        priority=0.9,
+        changefreq='daily'
+    ),
     'column': GenericSitemap(
-                {
-                    'queryset': Column.objects.all()
-                },
-                priority=0.9,
-                changefreq='daily'
-            ),
+        {
+            'queryset': Column.objects.all()
+        },
+        priority=0.9,
+        changefreq='daily'
+    ),
     'static': StaticViewSitemap
 }
 
